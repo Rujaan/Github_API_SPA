2024009d59422d661dda59fca0b67c59210c5986
Rujan Shrestha <rujan209@gmail.com>
2022-02-16 22:27:52 +0545

    completed getting values through api

--- frontend/src/components/results.js
+++ frontend/src/components/results.js
@@ -2,9 +2,7 @@ import React from "react";
 
 const Result = ({ isLoading, repo }) => {
   return (
-    <div className="flex flex-col mt-10">
-      <br />
-      <div>loading</div>
+    <div className="flex flex-col mt-2">
       {isLoading && <div>loading</div>}
       {repo === [] ? (
         <div></div>
@@ -15,10 +13,29 @@ const Result = ({ isLoading, repo }) => {
       )}
       {repo.map((repo) => {
         return (
-          <li key={repo.id} className="repo_item">
-            <a href={repo.html_url} target="_blank">
-              {repo.name}
+          <li key={repo.id} className="p-2">
+            <p>forks:{repo.forks}</p>
+            <p>stars:{repo.stargazers_count}</p>
+            <p>watchers:{repo.watchers_count}</p>
+            <p>description:{repo.description}</p>
+            <p>issues:{repo.open_issues_count}</p>
+            <p>last updated:{repo.updated_at}</p>
+            <p>default Branch:{repo.default_branch}</p>
+            <p>visibility : {repo.visibility}</p>
+            <p>allow forking: {repo.allow_forking}</p>
+            <p>home page:{repo.homepage}</p>
+            <img src={repo.owner.avatar_url} alt="" />
+            <a
+              href={"https:www.github.com/" + repo.owner.login}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Owner Name:{repo.owner.login}
             </a>
+            <a href={repo.html_url} target="_blank" rel="noreferrer">
+              Repo Name:{repo.name}
+            </a>
+
             <p>{repo.description}</p>
           </li>
         );
